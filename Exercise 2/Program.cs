6fad68a327c0fd0477de68c975ccd30825b9475e
Aram <armsir-8182@mail.ru>
2022-10-14 11:08:35 +0300

    Some changes in first program

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -6,6 +6,6 @@ int a = int.Parse(Console.ReadLine()!);
 Console.Write("Enter the second number b= ");
 int b = int.Parse(Console.ReadLine()!);
 
-if (a > b) {Console.WriteLine("The biggest number is: a= " + a); }
-else if (a < b) {Console.WriteLine("The biggest number is: b= " + b); }
+if (a > b) {Console.WriteLine("The biggest number is: max=a= " + a); }
+else if (a < b) {Console.WriteLine("The biggest number is: max=b= " + b); }
        else {Console.WriteLine("The numbers are equal: a = b !"); }
