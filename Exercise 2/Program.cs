ff81724bc999d0931401d1902c7454eb5d977eed
Aram <armsir-8182@mail.ru>
2022-10-14 10:58:16 +0300

    Adding all files in Exercise 2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Написать программу,который на вход принимает 2 числа и выдает,
+какое число больше,какое меньше или они равны. */
+
+Console.Write("Enter the first number a= ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the second number b= ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b) {Console.WriteLine("The biggest number is: a= " + a); }
+else if (a < b) {Console.WriteLine("The biggest number is: b= " + b); }
+       else {Console.WriteLine("The numbers are equal: a = b !"); }
