66bd9f0e92eef8de5313e27fea59eecba0ba6903
Aram <armsir-8182@mail.ru>
2022-10-15 10:54:50 +0300

    added the program to find max number(defficult way)

---
+++ Exercise 4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу,которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.Write("Enter the first number: a= ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the second number: b= ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the third number: c= ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+
+if (b > max) {max = b; if (c > max) {max = c; Console.Write("The maximum number is: max=c= " + max ); }
+                   else {max = b; Console.Write("The maximum number is: max=b= " + max); } }
+else if (c > max) {max = c; Console.Write("The maximum number is: max=c= " + max); }
+     else {Console.Write("The maximum number is: max=a= " + max); }
