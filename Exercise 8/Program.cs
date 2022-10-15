d9ade40f946d2b78ec42954599ab0a93a5c7f22a
Aram <armsir-8182@mail.ru>
2022-10-15 13:40:32 +0300

    The Program writes all even numbers from 1 to N]

---
+++ Exercise 8/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Написать программу,которая на вход принимает число(N),
+а на выходе показывает все четные числа от 1 до N. */
+
+Console.Write("Enter any number N= ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while(i <= N)
+{if (i % 2 == 0) {Console.Write(i + ", " ); }
+i = i + 1;
+}
