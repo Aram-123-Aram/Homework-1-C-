0cd47af76c6f917718f13b7c636d3a8908d85934
Aram <armsir-8182@mail.ru>
2022-10-15 12:37:29 +0300

    Added a program,that decided number a is an even or an odd number

---
+++ Exercise 6/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Написать программу,которая на вход принимает число и выдает,
+является ли число четным(делится ли оно на два без остатка). */
+
+Console.Write("Enter the number a= ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 2 == 0) {Console.Write("Yes,the a is an even number !"); }
+else {Console.Write("No,the a isn't an even number, it is an odd number !"); }
